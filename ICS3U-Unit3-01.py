35dbf7a5345234ba7407fb094733b5915f8ca656
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-10-12 15:54:31 +0000

    done

---
+++ ICS3U-Unit3-01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Created by: Emmanuel
+# Created on: Sep 2022
+# This program calculates the area and perimeter of a circle
+#    with radius inputted from the user
+
+import math
+
+
+def main():
+    # this function adds two numbers together
+
+    # input
+    Fn = int(input("Enter in any number:"))
+    Sn = int(input("Enter in any number:"))
+
+    # process
+    Fn = (Fn + Sn)
+    Sn = (Fn + Sn)
+
+    # output
+    print("")
+    print("Total is: {0} ".format(Fn))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
